5bb92958678b1cad171c76ea75c95c65ee930597
Samreen Asif <161675789+SamreenAsif123@users.noreply.github.com>
2024-03-06 14:38:19 +0500

    Add files via upload
    Comparison Operator with code example

---
+++ Basic JavaScript/file9.js
@@ -0,0 +1,22 @@
+//Comparison Operator
+
+let  a = 3
+     b = 4
+     //To check  greater or smaller value
+     console.log(a>b);
+     console.log(a<b);
+     console.log(a<2);
+     console.log(b<8);
+     //To check  values are  equal or not 
+     console.log(a<=3);
+     console.log(b<=4)
+     console.log(a>=4)
+
+
+let p='c'
+    q='d'
+    console.log(p<q);
+
+let m='c'
+    n='c'
+    console.log(m>=n);
