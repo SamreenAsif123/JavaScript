dd5ec093b3ed8abd73a3322e3ebf622ceb087328
Samreen Asif <161675789+SamreenAsif123@users.noreply.github.com>
2024-03-07 12:57:22 +0500

    Add files via upload
    Logical Operators (And),(OR), (Not)

---
+++ Basic JavaScript/file10.js
@@ -0,0 +1,20 @@
+//Logical operators in JavaScript are used to perform logical operations on boolean values. There are three main logical operators: && (logical AND), || (logical OR), and ! (logical NOT).
+
+//Logical AND (&&): Returns true if both operands are true, otherwise, it returns false.
+let x = true;
+let y = false;
+console.log(x && y); // Outputs false
+
+
+//Logical OR (||): Returns true if at least one operand is true, otherwise, it returns false
+let a = true;
+let b = false;
+console.log(x || y); // Outputs true
+
+
+//Logical NOT (!): Returns the opposite boolean value of the operand.
+let m = true;
+console.log(!x); // Outputs false
+
+
+
