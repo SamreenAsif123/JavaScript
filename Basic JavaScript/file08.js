bc317486918b250b9e4bee342a59c5c6871a3058
Samreen Asif <161675789+SamreenAsif123@users.noreply.github.com>
2024-03-07 12:47:16 +0500

    Add files via upload
    This file contains JavaScript code showcasing Equality Operator adn In equality operator

---
+++ Basic JavaScript/file08.js
@@ -0,0 +1,42 @@
+//Equality Oeprators
+//(==)----equality operator (Checks if the values on both sides are equal (with type coercion).)
+let a= 4
+    b= 6
+    console.log(a==b); //false (because values are not similar)
+
+let c=5
+    d=5
+    console.log(c==d);//True (because values are similar)
+
+//(===)---Strict equality operator (Checks if the values on both sides are equal and of the same type (without type coercion).)
+let  e=5
+     f='5'
+ console.log(e===f);
+
+let  ef=5
+     fe=5
+     console.log(ef===fe);
+
+
+
+// (!=)----In-equality Operator (Checks if the values on both sides are not equal.)
+ let m=5
+     n=8
+     console.log(m!=n);
+
+ let mn=5
+     nm='5'
+     console.log(mn!=nm);
+
+
+//(!==)------Strict In-equality Oeprator (Checks if the values on both sides are not equal or not of the same type.)
+let p=3
+    q='3'
+    console.log(p!==q);
+
+let pq=3
+    qp=3
+    console.log(pq!==qp);
+
+
+
