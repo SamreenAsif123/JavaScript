75efcd9a1ac605587b606c660d30df0f2ea0a524
Samreen Asif <161675789+SamreenAsif123@users.noreply.github.com>
2024-03-07 12:35:13 +0500

    Add files via upload
     Primitive data types in JavaScript, including Number, String, Boolean, Null, Undefined, Symbol, and BigInt. Each type is illustrated with concise code examples to enhance understanding.

---
+++ Basic JavaScript/file03.js
@@ -0,0 +1,44 @@
+//primitive data types
+
+//1: number
+//Represents numeric values.
+//Can be integers or floating-point numbers.
+
+let age= "23";
+    height="5.4";
+
+//2:String
+//Represents text.
+
+let myName= "Samreen";
+    degreeName="Biotechnology";
+
+//3:Boolean
+//Represents true or false values.
+
+let isfollow= "true";
+    hasPermission="false";
+
+//4:Null
+//Represents an intentional absence of any value.
+//It is explicitly assigned to a variable.
+
+let result= null;
+
+//5:Undefined
+//Represents a variable that has been declared but not assigned a value.
+
+let address;
+
+//5:Symbol
+//Represents a unique identifier.
+
+const key = Symbol("unique");
+
+//6: BigInt
+//Represents whole numbers larger than Number.MAX_SAFE_INTEGER.
+
+const bigNumber = "1234567890123456789012345678901234567890n";
+
+
+    
